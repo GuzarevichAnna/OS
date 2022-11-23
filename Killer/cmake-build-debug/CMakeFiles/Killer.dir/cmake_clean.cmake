e2bcb18755b2ae2c2d32c751385e6aeefdd1557c
Anna Guzarevich <annaguzarevichprogr@gmail.com>
2022-11-23 17:00:45 +0300

    initial commit

---
+++ Killer/cmake-build-debug/CMakeFiles/Killer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Killer.dir/main.cpp.obj"
+  "Killer.exe"
+  "Killer.exe.manifest"
+  "Killer.pdb"
+  "libKiller.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Killer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
